2e3dbe74941f113717a228f57cc8a53fb275bd00
andrew <andrewasmit@hotmail.com>
2023-03-31 13:44:44 -0500

    commit tree 9d3a035357cda4bbadbb08e86c0d81d87c7efe93

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,22 @@
 class Tree
+
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id elem_id
+    result = []
+    nodes_to_visit = [@root]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      if node[:id] == elem_id
+        result.push(node)
+      else
+        nodes_to_visit = node[:children] + nodes_to_visit
+      end
+    end
+    result[0]
+  end
+
 end
